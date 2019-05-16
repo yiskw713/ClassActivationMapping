7d5c95d299892075453aead3287721b0e3d6fe4b
Yuchi Ishikawa <yuchi@yiskw.local>
2019-05-16 09:48:27 +0900

    simplify the repository

--- cam.py
+++ cam.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 
 
@@ -22,11 +21,10 @@ class SaveValues():
         self.backward_hook.remove()
 
 
-""" Class Activation Mapping """
-
-
 class CAM(object):
-    def __init__(self, model, target_layer_obj, target_layer_aff):
+    """ Class Activation Mapping """
+
+    def __init__(self, model, target_layer):
         """
         Args:
             model: ResNet_linear()
@@ -34,68 +32,51 @@ class CAM(object):
         """
 
         self.model = model
-        self.target_layer_obj = target_layer_obj
-        self.target_layer_aff = target_layer_aff
+        self.target_layer = target_layer
 
         # save values of activations and gradients in target_layer
-        self.values_obj = SaveValues(self.target_layer_obj)
-        self.values_aff = SaveValues(self.target_layer_aff)
+        self.values = SaveValues(self.target_layer)
 
     def forward(self, x):
         """
         Args:
             x: input image. shape =>(1, 3, H, W)
         Return:
-            heatmap: class activation mappings of predicted classes
-                    [{obj_id: cam}, {aff_id1: cam1, aff_id2: cam2, ...}]
+            heatmap: class activation mappings of the predicted class
         """
 
-        pred_obj, pred_aff = self.model(x)
-
         # object classification
-        pred_obj = torch.sigmoid(pred_obj)
-        pred_obj[pred_obj > 0.5] = 1
-        pred_obj[pred_obj <= 0.5] = 0
+        score = self.model(x)
+        prob = F.softmax(score, dim=1)
+        max_prob, idx = torch.max(prob, dim=1)
+        print(
+            "predicted object ids {}\t probability {}".format(idx.item(), max_prob.item()))
 
-        # affordance classification
-        pred_aff = torch.sigmoid(pred_aff)
-        pred_aff[pred_aff > 0.5] = 1
-        pred_aff[pred_aff <= 0.5] = 0
+        # cam can be calculated from the weights of linear layer and activations
+        weight_fc = list(
+            self.model._modules.get('fc').parameters())[0].to('cpu').data
+        cam = self.getCAM(self.values, weight_fc, idx.item())
 
-        print("predicted object ids {}".format(pred_obj))
-        print("predicted affordance ids {}".format(pred_aff))
-
-        weight_fc_obj = list(self.model._modules.get('fc_obj').parameters())[0].to('cpu').data
-        weight_fc_aff = list(self.model._modules.get('fc_aff').parameters())[0].to('cpu').data
-
-        cams_obj = dict()
-        cams_aff = dict()
-
-        for i in pred_obj.nonzero():
-            cam = self.getCAM(self.values_obj, weight_fc_obj, i)
-            cams_obj[i[1].item()] = cam    # i[i] is object id
-
-        for i in pred_aff.nonzero():
-            cam = self.getCAM(self.values_aff, weight_fc_aff, i)
-            cams_aff[i[1].item()] = cam.data    # i[i] is affordance id
-
-        return cams_obj, cams_aff
+        return cam
 
     def __call__(self, x):
         return self.forward(x)
 
-    def getCAM(self, values, weight_fc, index):
+    def getCAM(self, values, weight_fc, idx):
         '''
         values: the activations and gradients of target_layer
-        activations: feature map before GAP.  shape => (N, C, H, W)
+            activations: feature map before GAP.  shape => (1, C, H, W)
         weight_fc: the weight of fully connected layer.  shape => (num_classes, C)
-        cam: class activation map.  shape=> (N, num_classes, H, W)
+        idx: predicted class id
+        cam: class activation map.  shape => (1, num_classes, H, W)
         '''
 
         cam = F.conv2d(values.activations, weight=weight_fc[:, :, None, None])
         _, _, h, w = cam.shape
 
-        cam = cam[index[0], index[1], :, :]
+        # class activation mapping only for the predicted class
+        # cam is normalized with min-max.
+        cam = cam[:, idx, :, :]
         cam -= torch.min(cam)
         cam /= torch.max(cam)
         cam = cam.view(1, 1, h, w)
@@ -103,70 +84,59 @@ class CAM(object):
         return cam.data
 
 
-""" Grad CAM """
-
-
 class GradCAM(CAM):
-    """
-    Args:
-        model: ResNet_linear()
-        target_layer: conv_layer before Global Average Pooling
-    """
+    """ Grad CAM """
 
-    def __init__(self, model, target_layer_obj, target_layer_aff):
-        super().__init__(model, target_layer_obj, target_layer_aff)
+    def __init__(self, model, target_layer):
+        super().__init__(model, target_layer)
+
+        """
+        Args:
+            model: a base model to get CAM, which need not have global pooling and fully connected layer.
+            target_layer: conv_layer you want to visualize
+        """
 
     def forward(self, x):
         """
         Args:
             x: input image. shape =>(1, 3, H, W)
         Return:
-            heatmap: class activation mappings of predicted classes
-                    [{obj_id: cam}, {aff_id1: cam1, aff_id2: cam2, ...}]
+            heatmap: class activation mappings of the predicted class
         """
-
-        score_obj, score_aff = self.model(x)
-
         # object classification
-        pred_obj = torch.sigmoid(score_obj)
-        pred_obj[pred_obj > 0.5] = 1
-        pred_obj[pred_obj <= 0.5] = 0
+        score = self.model(x)
+        prob = F.softmax(score)
+        max_prob, idx = torch.max(prob)
+        print(
+            "predicted object ids {}\t probability {}".format(idx.item(), max_prob.item()))
 
-        # affordance classification
-        pred_aff = torch.sigmoid(score_aff)
-        pred_aff[pred_aff > 0.5] = 1
-        pred_aff[pred_aff <= 0.5] = 0
+        # caluculate cam of the predicted class
+        cam = self.getGradCAM(self.values, score, idx.item())
 
-        print("predicted object ids {}".format(pred_obj))
-        print("predicted affordance ids {}".format(pred_aff))
-
-        cams_obj = dict()
-        cams_aff = dict()
-
-        # caluculate cam of each predicted class
-        for i in pred_obj.nonzero():
-            cam = self.getGradCAM(self.values_obj, score_obj, i)
-            cams_obj[i[1].item()] = cam
-
-        for i in pred_aff.nonzero():
-            cam = self.getGradCAM(self.values_aff, score_aff, i)
-            cams_aff[i[1].item()] = cam
-
-        return cams_obj, cams_aff
+        return cam
 
     def __call__(self, x):
         return self.forward(x)
 
-    def getGradCAM(self, values, score, index):
+    def getGradCAM(self, values, score, idx):
+        '''
+        values: the activations and gradients of target_layer
+            activations: feature map before GAP.  shape => (1, C, H, W)
+        score: the output of the model before softmax
+        idx: predicted class id
+        cam: class activation map.  shape=> (1, 1, H, W)
+        '''
+
         self.model.zero_grad()
-        score[index[0], index[1]].backward(retain_graph=True)
+        score[0, idx].backward(retain_graph=True)
         activations = values.activations
         gradients = values.gradients
         n, c, _, _ = gradients.shape
         alpha = gradients.view(n, c, -1).mean(2)
         alpha = alpha.view(n, c, 1, 1)
 
-        cam = (alpha * activations).sum(dim=1, keepdim=True)    # shape => (1, 1, H', W')
+        # shape => (1, 1, H', W')
+        cam = (alpha * activations).sum(dim=1, keepdim=True)
         cam = F.relu(cam)
         cam -= torch.min(cam)
         cam /= torch.max(cam)
@@ -175,14 +145,15 @@ class GradCAM(CAM):
 
 
 class GradCAMpp(GradCAM):
-    """
-    Args:
-        model: ResNet_linear()
-        target_layer: conv_layer before Global Average Pooling
-    """
+    """ Grad CAM plus plus """
 
-    def __init__(self, model, target_layer_obj, target_layer_aff):
-        super().__init__(model, target_layer_obj, target_layer_aff)
+    def __init__(self, model, target_layer):
+        super().__init__(model, target_layer)
+        """
+        Args:
+            model: a base model
+            target_layer: conv_layer you want to visualize
+        """
 
     def forward(self, x):
         """
@@ -190,44 +161,34 @@ class GradCAMpp(GradCAM):
             x: input image. shape =>(1, 3, H, W)
         Return:
             heatmap: class activation mappings of predicted classes
-                    [{obj_id: cam}, {aff_id1: cam1, aff_id2: cam2, ...}]
         """
 
-        score_obj, score_aff = self.model(x)
-
         # object classification
-        pred_obj = torch.sigmoid(score_obj)
-        pred_obj[pred_obj > 0.5] = 1
-        pred_obj[pred_obj <= 0.5] = 0
+        score = self.model(x)
+        prob = F.softmax(score)
+        max_prob, idx = torch.max(prob)
+        print(
+            "predicted object ids {}\t probability {}".format(idx.item(), max_prob.item()))
 
-        # affordance classification
-        pred_aff = torch.sigmoid(score_aff)
-        pred_aff[pred_aff > 0.5] = 1
-        pred_aff[pred_aff <= 0.5] = 0
+        # caluculate cam of the predicted class
+        cam = self.getGradCAMpp(self.values, score, idx.item())
 
-        print("predicted object ids {}".format(pred_obj))
-        print("predicted affordance ids {}".format(pred_aff))
-
-        cams_obj = dict()
-        cams_aff = dict()
-
-        # caluculate cam of each predicted class
-        for i in pred_obj.nonzero():
-            cam = self.getGradCAMpp(self.values_obj, score_obj, i)
-            cams_obj[i[1].item()] = cam
-
-        for i in pred_aff.nonzero():
-            cam = self.getGradCAMpp(self.values_aff, score_aff, i)
-            cams_aff[i[1].item()] = cam
-
-        return cams_obj, cams_aff
+        return cam
 
     def __call__(self, x):
         return self.forward(x)
 
     def getGradCAMpp(self, values, score, index):
+        '''
+        values: the activations and gradients of target_layer
+            activations: feature map before GAP.  shape => (1, C, H, W)
+        score: the output of the model before softmax. shape => (1, n_classes)
+        idx: predicted class id
+        cam: class activation map.  shape=> (1, 1, H, W)
+        '''
+
         self.model.zero_grad()
-        score[index[0], index[1]].backward(retain_graph=True)
+        score[0, index[1]].backward(retain_graph=True)
         activations = values.activations
         gradients = values.gradients
         n, c, _, _ = gradients.shape
@@ -235,15 +196,17 @@ class GradCAMpp(GradCAM):
         # calculate alpha
         numerator = gradients.pow(2)
         denominator = 2 * gradients.pow(2)
-        denominator += (activations * gradients.pow(3)).view(n, c, -1).sum(-1, keepdim=True).view(n, c, 1, 1)
+        ag = activations * gradients.pow(3)
+        denominator += ag.view(n, c, -1).sum(-1, keepdim=True).view(n, c, 1, 1)
         denominator = torch.where(
             denominator != 0.0, denominator, torch.ones_like(denominator))
         alpha = numerator / (denominator + 1e-7)
 
-        relu_grad = F.relu(score[index[0], index[1]].exp() * gradients)
+        relu_grad = F.relu(score[0, index[1]].exp() * gradients)
         weights = (alpha * relu_grad).view(n, c, -1).sum(-1).view(n, c, 1, 1)
 
-        cam = (weights * activations).sum(1, keepdim=True)    # shape => (1, 1, H', W')
+        # shape => (1, 1, H', W')
+        cam = (weights * activations).sum(1, keepdim=True)
         cam = F.relu(cam)
         cam -= torch.min(cam)
         cam /= torch.max(cam)
--- visualize.py
+++ visualize.py
@@ -1,38 +1,15 @@
 import torch
 import torch.nn.functional as F
 
-from torch.utils.data import DataLoader
-from torchvision.utils import save_image, make_grid
-from torchvision import transforms
-
-import argparse
 import numpy as np
 import cv2
-import yaml
-
-from addict import Dict
-
-from dataset import PartAffordanceDataset, ToTensor, Resize
-from dataset import CenterCrop, Normalize, reverse_normalize
-from model.resnet import ResNet50_convcam, ResNet50_linearcam, ResNet152_linearcam2
-from model.unet import UNet
-from model.deeplabv2_linear import DeepLabV2_linear, DeepLabV2_linear_max
-from cam import CAM, GradCAM, GradCAMpp
 
 
-def get_arguments():
-    '''
-    parse all the arguments from command line inteface
-    return a list of parsed arguments
-    '''
-
-    parser = argparse.ArgumentParser(
-        description='adversarial learning for affordance detection')
-    parser.add_argument('config', type=str, help='path of a config file')
-    parser.add_argument('--device', type=str, default='cpu',
-                        help='choose a device you want to use')
-
-    return parser.parse_args()
+def reverse_normalize(x, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    x[:, 0, :, :] = x[:, 0, :, :] * std[0] + mean[0]
+    x[:, 1, :, :] = x[:, 1, :, :] * std[1] + mean[1]
+    x[:, 2, :, :] = x[:, 2, :, :] * std[2] + mean[2]
+    return x
 
 
 def visualize(img, cam):
@@ -46,7 +23,7 @@ def visualize(img, cam):
     """
 
     _, _, H, W = img.shape
-    cam = F.interpolate(cam, size=(H, W), mode='bilinear')
+    cam = F.interpolate(cam, size=(H, W), mode='bilinear', align_corners=False)
     cam = 255 * cam.squeeze()
     heatmap = cv2.applyColorMap(np.uint8(cam), cv2.COLORMAP_JET)
     heatmap = torch.from_numpy(heatmap.transpose(2, 0, 1))
@@ -58,101 +35,3 @@ def visualize(img, cam):
     result = result.div(result.max())
 
     return result
-
-
-def main():
-
-    args = get_arguments()
-
-    # configuration
-    CONFIG = Dict(
-        yaml.safe_load(open(args.config)))
-
-    """ DataLoader """
-    test_transform = transforms.Compose([
-        CenterCrop(CONFIG),
-        Resize(CONFIG),
-        ToTensor(CONFIG),
-        Normalize()
-    ])
-
-    test_data = PartAffordanceDataset(
-        CONFIG.test_data, config=CONFIG, transform=test_transform)
-
-    test_loader = DataLoader(
-        test_data, batch_size=1, shuffle=True, num_workers=1)
-
-    test_iter = iter(test_loader)
-
-    """ Load Model """
-    if CONFIG.model == "ResNet50_convcam":
-        model = ResNet50_convcam(CONFIG.obj_classes, CONFIG.aff_classes)
-    elif CONFIG.model == "ResNet50_linearcam":
-        model = ResNet50_linearcam(CONFIG.obj_classes, CONFIG.aff_classes)
-    elif CONFIG.model == "ResNet152_linearcam2":
-        model = ResNet152_linearcam2(CONFIG.obj_classes, CONFIG.aff_classes)
-    elif CONFIG.model == "UNet":
-        model = UNet(CONFIG.obj_classes, CONFIG.aff_classes)
-    elif CONFIG.model == "DeepLabV2_linear":
-        model = DeepLabV2_linear(
-            CONFIG.obj_classes, CONFIG.aff_classes, n_blocks=[3, 4, 23, 3], atrous_rates=[6, 12, 18, 24]
-        )
-    elif CONFIG.model == "DeepLabV2_linear_max":
-        model = DeepLabV2_linear_max(
-            CONFIG.obj_classes, CONFIG.aff_classes, n_blocks=[3, 4, 23, 3], atrous_rates=[6, 12, 18, 24]
-        )
-    else:
-        print('ResNet50_linearcam will be used.')
-        model = ResNet50_linearcam(CONFIG.obj_classes, CONFIG.aff_classes)
-
-    state_dict = torch.load(CONFIG.result_path + '/best_accuracy_model.prm',
-                            map_location=lambda storage, loc: storage)
-    model.load_state_dict(state_dict)
-    model.eval()
-
-    # target_layer_obj = model.conv_obj
-    # target_layer_aff = model.conv_aff
-
-    # choose CAM, GradCAM or GradCMApp
-    # wrapped_model = CAM(model, target_layer_obj, target_layer_aff)
-    # wrapped_model = GradCAM(model, target_layer_obj, target_layer_aff)
-    # wrapped_model = GradCAMpp(model, target_layer_obj, target_layer_aff)
-    wrapped_model = CAM(model, model.feature, model.feature)
-    cnt = 0
-    while True:
-        print('\n************ loading image ************\n')
-        sample = test_iter.next()
-        img = sample['image']
-        print("object ids {}\n".format(sample['obj_label'].nonzero()))
-        print("affordance ids {}\n".format(sample['aff_label'].nonzero()))
-
-        # calculate cams
-        cams_obj, cams_aff = wrapped_model(img)
-
-        img = reverse_normalize(img)
-        images = []    # save an input image and synthesized images with cams
-        images.append(img)
-
-        for key, val in cams_obj.items():
-            result = visualize(img, val)
-            images.append(result)
-
-        for key, val in cams_aff.items():
-            result = visualize(img, val)
-            images.append(result)
-
-        images = make_grid(torch.cat(images, 0))
-        save_image(images, CONFIG.result_path + '/result{}.png'.format(cnt))
-
-        print('\nIf you want to quit, please press q. Else, press the others\n')
-        # i = input()
-        # if i == 'q':
-        #     break
-        cnt += 1
-
-        if cnt == 50:
-            break
-
-
-if __name__ == '__main__':
-    main()
